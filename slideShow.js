ec01de765d8bb44f1969e9485a2ce0706f6634f1
Greg Dotson <126525784+Coach-Greg@users.noreply.github.com>
2023-04-01 19:54:04 -0700

    Update slideShow.js

--- slideShow.js
+++ slideShow.js
@@ -1,5 +1,5 @@
 var imgItem = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19'];
-var imgPath = "source/slideshow/";
+var imgPath = "source/22-23-Slideshow/";
 var imgExtension = ".jpg";
 
 
